b3a857e589425ec4f6ddad08f567058c904206c8
Natalia Grzesikowska <nagrzesikowska@gmail.com>
2021-12-01 21:31:39 +0100

    add weather icon to main temperature board

--- src/index.js
+++ src/index.js
@@ -40,6 +40,8 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+    document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather.[0].icon}@2x.png`);
+
 }
 
 function searchCity(city) {
