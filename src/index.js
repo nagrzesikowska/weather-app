35d81c26348334d28e662c048365e91df75babc5
Natalia Grzesikowska <nagrzesikowska@gmail.com>
2021-12-09 10:36:20 +0100

    function displayForecast

--- src/index.js
+++ src/index.js
@@ -30,9 +30,11 @@ function formatDate() {
 let currentDateandTime = document.querySelector("#dateNow");
 currentDateandTime.innerHTML = formatDate();
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+  let days = ["Monday", "Tuesday", "Wednesday", "Sunday"];
 
   let forecastHTML = `<div class="row">`;
   days.forEach(function (day) {
@@ -43,15 +45,21 @@ function displayForecast() {
               <div class="day">${day}</div>
               <div class="forecast-temerature">15/2°C</div>
               <img src="jpg/chill.jpg" width="55" />
-            </div>
-        `;
+          
+        </div>`;
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "1cc73f07fc9c0b52b0f5b6369e133a2a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
-  console.log(response);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -66,6 +74,7 @@ function displayWeatherCondition(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -91,7 +100,7 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
-displayForecast();
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
